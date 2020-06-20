6f963fb2f53b53845ba54475622c3d89d910c912
Aika Okemoto <aika2004oke@gmail.com>
2020-06-20 19:12:35 +0900

    Initial Commit

---
+++ Janken/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Janken
+//
+//  Created by 桶本あいか on 2020/06/20.
+//  Copyright © 2020 Okemoto.Aika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
